8e37fd684703b3172414683fa04f0f3fcc041c1e
araiseiya-cmyk <arai_seiya@keio.jp>
2026-07-01 03:11:18 +0000

    Complete age update

--- index.ts
+++ index.ts
@@ -1,32 +1,35 @@
 import "dotenv/config";
 import express from "express";
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client";
 
-// 接続設定
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter, log: ["query"] });
+
 const app = express();
 const PORT = process.env.PORT || 8888;
 
-// EJS の設定
 app.set("view engine", "ejs");
 app.set("views", "./views");
-// フォームの入力を受け取るための設定
 app.use(express.urlencoded({ extended: true }));
 
-// 一覧表示
 app.get("/", async (req, res) => {
   const users = await prisma.user.findMany();
   res.render("index", { users });
 });
 
-// ユーザー追加
 app.post("/users", async (req, res) => {
-  const name = req.body.name;
+  const { name, age } = req.body;
   if (name) {
-    const newUser = await prisma.user.create({ data: { name } });
-    console.log("追加:", newUser);
+    await prisma.user.create({
+      data: {
+        name: name,
+        // 年齢があれば数値に変換し、なければ null にする
+        age: age ? parseInt(age, 10) : null
+      }
+    });
   }
   res.redirect("/");
 });
